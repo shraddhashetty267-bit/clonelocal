9284e1f8484ed3eb2c41f7ebc84d5db9f364e7cc
shraddhashetty267-bit <shraddhashetty267@gmail.com>
2026-08-25 11:24:03 +0530

    Update name1.py

--- name1.py
+++ name1.py
@@ -1,7 +1,7 @@
 name=input("Enter your name:")
 surname=input("Enter your surname:")
-contact=input("Enter your contact number")
-city=input("Enter your city")
+contact=input("Enter your contact number:")
+city=input("Enter your city:")
 
 print("your fullname is:",name+" "+surname)
 print("your contact is:",contact)
